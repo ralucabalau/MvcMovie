bc8fd7f18d4f7ca19bc9cda65941d971e8cbad77
ralu_b <ralubalau22@gmail.com>
2024-05-15 21:07:41 +0300

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View(); //Calls the controller's View method.
+                       //Uses a view template to generate an HTML response.
+    }
+
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+    //Uses HtmlEncoder.Default.Encode to protect the app from malicious input, such as through JavaScript.
+
+    // Every public method in a controller is callable as an HTTP endpoint.In the sample above, both methods return a string.
+}
